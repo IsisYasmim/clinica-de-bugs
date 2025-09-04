06b550ecbf9abd7073282a0ad467b0b5ea97f111
Eduardo Mendes <emendes@alunos.utfpr.edu.br>
2025-09-04 08:55:45 -0300

    Adicionando as primeiras 5 atividades

---
+++ Problema-2-Escolha_De_Tipos_De_Dados/Ecolha_De_Tipos_De_Dados.cs
@@ -0,0 +1,19 @@
+﻿//Acrescentado um suficoo 'f' para indicar uma literal float.
+//Retirado as aspas duplas do valor atribuido a variavel do tipo devido erro de sintaxe.
+//Adicionado aspas duplas na váriavel do tipo string.
+//Adicioado ponto e virugula no final da atribuição da variável do tipo char.
+//Adicionado aspas simples na atribuição da variável do tipo char.
+double notaProva = 8.12345678910;
+int quantidadeAlunos = 42;
+string nomeCurso = "Fundamentos de C#";
+bool pagamentoEfetuado = false;
+char digitoVerificador = '7';
+
+Console.WriteLine(notaProva);
+Console.WriteLine(quantidadeAlunos);
+//Nome da váriavel nomeCurso corrigida.
+//Antes: Console.WriteLine(nomecurso);
+//Agora:
+Console.WriteLine(nomeCurso);
+Console.WriteLine(pagamentoEfetuado);
+Console.WriteLine(digitoVerificador);
