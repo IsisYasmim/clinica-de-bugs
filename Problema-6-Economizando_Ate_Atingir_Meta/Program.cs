a7d31d86ae65117f693869ac3cf8841e0c40edce
IsisYasmim <isis55.yasmim@gmail.com>
2025-09-03 21:01:21 -0300

    Adicionando exercicio 6

---
+++ Problema-6-Economizando_Ate_Atingir_Meta/Program.cs
@@ -0,0 +1,13 @@
+﻿double total = 0; // Valores atribuídos a esta variável devem ser do tipo double.
+while (total < 50)
+{
+    Console.Write("Digite o valor a ser guardado: ");
+    string? input = Console.ReadLine();
+    
+    if (input is not null) // Verifica se a entrada não é nula antes de tentar usá-la.
+    {
+        double valor = double.Parse(input.Replace('.', ','));
+        total += valor; // Adiciona o valor digitado ao total, e não o substitui.
+    }
+}
+Console.WriteLine($"Meta atingida! Total economizado: R$ {total}"); // Deve-se utilizar o símbolo $ para interpolação de strings.
