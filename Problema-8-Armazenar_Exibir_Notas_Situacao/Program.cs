1d6b77008f4c52bbfa786f786764fb01599fa96e
IsisYasmim <isis55.yasmim@gmail.com>
2025-09-03 21:59:22 -0300

    Adicionando restante do ex 8

--- Problema-8-Armazenar_Exibir_Notas_Situacao/Program.cs
+++ Problema-8-Armazenar_Exibir_Notas_Situacao/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] notas = new int[5];
+
+for (int i = 0; i < notas.Length; i++) // O loop deve iterar até o tamanho do array.
+{
+    Console.Write($"Digite a nota {i + 1}: ");
+    string? input = Console.ReadLine(); 
+    if (input is null) // Verifica se a entrada não é nula antes de tentar usá-la.
+    {
+        Console.WriteLine("Entrada inválida.");
+        return;
+    }
+    notas[i] = int.Parse(input);
+}
+
+for (int i = 0; i < notas.Length; i++)
+{
+    string situacao = notas[i] >= 7 ? "Aprovado": "Reprovado"; // Corrigido o operador ternário para refletir a lógica correta.
+    Console.WriteLine($"Nota (i + 1): {notas[i]} - {situacao}");
+}
